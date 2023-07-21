b55ebb68d5b2e1aaecf1bfddb00a8a502f99ff8e
chrissyhroberts <chrissyhroberts@yahoo.co.uk>
2023-07-21 14:58:50 +0100

    Update sendODK.py
    Adds a UI for sendODK.py
    Shows progress through list of submissions
    Summarises total number of submissions sent now
    and adds a count of all submissions ever sent (i.e. those in log.txt)

--- examples/sendODK.py
+++ examples/sendODK.py
@@ -3,24 +3,43 @@ import binascii
 import network
 import os
 import uos
+import time
+import badger2040
 
+# Initialize the Badger eINK display
+display = badger2040.Badger2040()
+display.led(128)
+display.set_update_speed(2)
+
+# Constants
+WIDTH = 250  # Width of the Badger2040 display
+HEIGHT = 122  # Height of the Badger2040 display
+
+# Your existing functions (connect_to_wifi, base64_encode, submit_submission, log_submission, walk, load_submitted_files, submit_xml_files_in_folder) go here...
 
 def connect_to_wifi(ssid, password):
+    # set up the screen with a black background and black pen
+    display.set_pen(0)  # Change this to 0 if a white background is used
+    display.clear()
+    display.set_pen(15)
+
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
 
     if wlan.isconnected():
-        print('Already connected to Wi-Fi')
+        display.text('Already connected to Wi-Fi', 10, 20)
+        display.update()
         return
 
     wlan.connect(ssid, password)
 
     while not wlan.isconnected():
-        pass
+        display.text('Connecting to Wi-Fi...', 10, 20)
+        display.update()
 
-    print('Connected to Wi-Fi')
-    print('Network config:', wlan.ifconfig())
-
+    display.text('Connected to Wi-Fi', 10, 20)
+    display.text('Network config:' + str(wlan.ifconfig()), 10, 60)
+    display.update()
 
 def base64_encode(string):
     # Encoding the string to bytes
@@ -33,7 +52,6 @@ def base64_encode(string):
     encoded_base64_string = encoded_base64_bytes.decode('utf-8').strip()
 
     return encoded_base64_string
-
 
 def submit_submission(file_path, url, username, password):
     with open(file_path, 'r') as file:
@@ -49,13 +67,14 @@ def submit_submission(file_path, url, username, password):
     response = urequests.post(url, headers=headers, data=xml_data)
 
     if response.status_code in (200, 201):
-        print('Submission successful')
+        display.text('Submission successful', 10, 60)
+        display.update()
         log_submission(file_path, success=True, response_text=response.text)
     else:
-        print('Submission failed with status code:', response.status_code)
-        print('Response:', response.text)
+        display.text('Submission failed: ' + str(response.status_code), 10, 40)
+        display.text('Response: ' + response.text, 10, 60)
+        display.update()
         log_submission(file_path, success=False, response_text=response.text)
-
 
 def log_submission(file_path, success, response_text):
     with open('log.txt', 'a') as log_file:
@@ -94,10 +113,22 @@ def load_submitted_files():
     print("Submitted files:", submitted_files)
     return submitted_files
 
+def count_unique_uuids(log_file_path):
+    unique_uuids = set()
+    if log_file_path in uos.listdir():
+        with open(log_file_path, 'r') as log_file:
+            for line in log_file:
+                file_path, _, response = line.strip().split(', ')
+                uuid = response.split('/')[-1]
+                unique_uuids.add(uuid)
+    return len(unique_uuids)
 
 def submit_xml_files_in_folder(folder_path, url, username, password):
     # Load the set of submitted files from the log
     submitted_files = load_submitted_files()
+
+    # Add a counter for successful submissions
+    successful_submissions = 0
 
     file_paths = walk(folder_path)
     for file_path in file_paths:
@@ -106,16 +137,45 @@ def submit_xml_files_in_folder(folder_path, url, username, password):
             filename = file_path.split('/')[-1]
             if filename not in submitted_files:
                 print('Submitting file:', file_path)
+
+                # Instead of printing, use the Badger eINK display to show the submission status.
+                display.set_pen(0)  # Change this to 0 if a white background is used
+                display.clear()
+                display.set_pen(15)
+                display.text('Submitting file:', 10, 40)
+                display.text(file_path, 10, 60)
+                display.update()
+
                 submit_submission(file_path, url, username, password)
                 print('---')
 
-                # Update the log only after a successful submission
+                # Update the log and counter only after a successful submission
                 if filename not in submitted_files:
                     log_submission(file_path, success=True, response_text='')
+                    successful_submissions += 1
 
                 submitted_files.add(filename)  # Update the set with the filename
 
+    # Get the total number of unique UUID numbers in the log file
+    unique_uuid_count = count_unique_uuids('log.txt')
+
+    # Display the total number of successful submissions
+    display.set_pen(0)  # Change this to 0 if a white background is used
+    display.clear()
+    display.set_pen(15)
+
+    display.text('Submissions Sent Now:', 10, 20)
+    display.text(str(successful_submissions), 10, 40)
+    display.text('Total Submissions :', 10, 60)
+    display.text(str(unique_uuid_count), 10, 80)
+    display.update()
+
     print("Submitted files:", submitted_files)
+
+
+##########################
+# MAIN
+##########################
 
 print("Connecting to Wi-Fi...")
 connect_to_wifi('YOURNETWORKSSID', 'YOURNETWORKPASSWORD")
